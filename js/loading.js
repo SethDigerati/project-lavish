2e7a2bcfcd1ed138794108a01c2a06bb6f178ebd
Seth Digerati <163216896+SethDigerati@users.noreply.github.com>
2024-05-15 01:20:35 +0300

    a ton of edits

--- js/loading.js
+++ js/loading.js
@@ -7,19 +7,12 @@ document.addEventListener('DOMContentLoaded', function () {
         // Animate loading screen off-screen upwards
         loadingScreen.style.transition = 'transform 1.5s cubic-bezier(0.25, 0.1, 0.25, 1), z-index 0s linear 1.5s';
         loadingScreen.style.transform = 'translateY(-100%)';
-        loadingScreen.style.zIndex = '2000'; // Ensure loading screen remains on top
     }, 6000);
 
     // Event listener for touchstart (or mousedown) to record initial position
     loadingScreen.addEventListener('touchstart', function (event) {
         startX = event.touches[0].clientX;
     });
-
-    // Event listener for touchmove (or mousemove) to update end position during swipe
-    loadingScreen.addEventListener('touchmove', function (event) {
-        endX = event.touches[0].clientX;
-    });
-
     // Event listener for touchend (or mouseup) to determine swipe direction and distance
     loadingScreen.addEventListener('touchend', function () {
         var swipeDistance = endX - startX;
@@ -28,8 +21,6 @@ document.addEventListener('DOMContentLoaded', function () {
         if (swipeDistance > 100) {
             // Animate loading screen off-screen upwards
             loadingScreen.style.transition = 'transform 1.5s cubic-bezier(0.25, 0.1, 0.25, 1), z-index 0s linear 1.5s';
-            loadingScreen.style.transform = 'translateY(-100%)';
-            loadingScreen.style.zIndex = '2000'; // Ensure loading screen remains on top
-        }
+            loadingScreen.style.transform = 'translateY(-100%)';        }
     });
 });
